0d82d1e5ef7880f35467278b6ce0286c6de74f0f
Austin-chia <e1525125@u.nus.edu>
2026-08-20 23:24:20 +0800

    Rename Duke template to Trayce

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Trayce User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Trayce.java
@@ -0,0 +1,14 @@
+public class Trayce {
+    public static void main(String[] args) {
+        String banner = " _____\n"
+                + "|_   _| __ __ _ _   _  ___ ___\n"
+                + "  | |  '__/ _` | | | |/ __/ _ \\\n"
+                + "  | | | | | (_| | |_| | (_|  __/\n"
+                + "  |_| |_|  \\__,_|\\__, |\\___\\___|\n"
+                + "                  __/ |\n"
+                + "                 |___/\n";
+        System.out.println(banner);
+        System.out.println("Hello! I'm Trayce. What can I do for you?\n");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
